ae0321917d757bfe70a114d042f23681d117f538
Dylan Fung <dilanfung@gmail.com>
2021-08-17 23:35:47 -0600

    Multiple changes

--- SistemaInventario/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ SistemaInventario/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -36,14 +36,14 @@ namespace SistemaInventario.Areas.Identity.Pages.Account
             var user = await _userManager.FindByIdAsync(userId);
             if (user == null)
             {
-                return NotFound($"Unable to load user with ID '{userId}'.");
+                return NotFound($"Imposible cargar el usuario con ese ID '{userId}'.");
             }
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ChangeEmailAsync(user, email, code);
             if (!result.Succeeded)
             {
-                StatusMessage = "Error changing email.";
+                StatusMessage = "Error al cambiar correo";
                 return Page();
             }
 
@@ -52,12 +52,12 @@ namespace SistemaInventario.Areas.Identity.Pages.Account
             var setUserNameResult = await _userManager.SetUserNameAsync(user, email);
             if (!setUserNameResult.Succeeded)
             {
-                StatusMessage = "Error changing user name.";
+                StatusMessage = "Error al cambiar usuario";
                 return Page();
             }
 
             await _signInManager.RefreshSignInAsync(user);
-            StatusMessage = "Thank you for confirming your email change.";
+            StatusMessage = "Gracias por confirmar el cambio de su correo electronico";
             return Page();
         }
     }
